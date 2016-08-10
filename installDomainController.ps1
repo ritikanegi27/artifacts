5980c8c7c7427dc50598ffbfdaf01400e1c38c66
ritikanegi27 <v-rineg@microsoft.com>
2016-08-10 17:47:49 +0530

    Add files via upload

---
+++ installDomainController.ps1
@@ -0,0 +1,13 @@
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "Win2012" `
+-DomainName "Devlab.com" `
+-DomainNetbiosName "Devlab" `
+-ForestMode "Win2012" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
